db848dd2cb094cee9935919ea1fabe864e24da70
EstherSF23 <esthersf2311@gmail.com>
2025-10-31 13:02:15 +0100

    Añado ultimo ejercicio

--- python-exercises/source/extract.py
+++ python-exercises/source/extract.py
@@ -1,9 +1,21 @@
-import pandas as pd
+# extract.py
 
-def load_sales():
-    df = pd.read_csv("source/sales.csv").drop_duplicates()
-    df.columns = df.columns.str.lower()
-    df = df.apply(lambda x: x.str.lower() if x.dtype == "object" else x)
-    df["total"] = df["quantity"] * df["price"]
-    print(df)
-    return df
+from source.logging import logger
+from source.utils import csv_to_df
+
+def read_csv(csv_file):
+    try:
+        with open(csv_file, "r") as f:
+            content = f.read()
+            logger.info(f"CSV file '{csv_file}' correctly read")
+            return content
+    except FileNotFoundError:
+        logger.error(f"Error: The file '{csv_file}' was not found.")
+    except PermissionError:
+        logger.error(f"Error: Permission denied when trying to read '{csv_file}'.")
+    except Exception as e:
+        logger.error(f"Unexpected error reading '{csv_file}': {e}")
+
+def read_csv_with_pd(csv_file):
+    return csv_to_df(csv_file)
+ 
